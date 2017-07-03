4049e3afda8184f3928da0c7e0a2292b30074ce8
tdarby <tramainedarby@gmail.com>
2017-07-03 15:13:52 -0400

    Update app.py
    Update message directly from github

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from random_words import RandomWords
 
 @route('/')
 def index():
-    return "<h1>Hey there Tramaine </h1>"
+    return "<h1>Hey there Tramaine - How are you? </h1>"
 
 
 # since this is a read only talk to the replicas
